bbf7ba9d1863800bd0b98018367bdb6ba3653dfa
bilele123123 <tple06203@gmail.com>
2023-06-18 19:40:12 -0700

    Add CSS styling to website, mobile compatibility and JS transition.

---
+++ app.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+    const elements = document.querySelectorAll('.container-dashboard > *');
+    elements.forEach(function (element) {
+        element.classList.add('fade-in');
+    });
+});
+

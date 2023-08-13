ada7db542736ad89b2d8cda378f405af4018e5b5
bilele123123 <tple06203@gmail.com>
2023-08-12 19:04:05 -0700

    Complete header portion of portfolio.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TypewriterComponent from './Typewriter';
 
 function Header() {
   return (
@@ -20,19 +21,26 @@ function Header() {
           </ul>
         </div>
       </nav>
-      <div className="border-t border-gray-300"></div>
-      <div className="flex items-center justify-between p-4">
-        <div>
-        <img src="https://github.com/bilele123123/Portfolio/blob/website-revamp/assets/my_picture.png?raw=true" alt="Profile" className="w-300 h-300 rounded-full"/>
+
+      <div className="border-t border-gray-300 "></div>
+
+      <div className="flex items-center justify-between p-10 mx-32">
+        <div class="relative">
+          <div class="animblob"></div>
+          <img src="https://github.com/bilele123123/Portfolio/blob/website-revamp/assets/profile.png?raw=true" alt="Profile" class="w-300 h-300 rounded-full z-10 relative" />
         </div>
-        <div className="text-center">
-          <p className="text-xl font-bold">Welcome!</p>
-          <p className="text-xl font-bold">My name is Thai, a Software Developer.</p>
-          <ul>
-            <li><a target="blank" className="link-background" href="https://drive.google.com/file/d/1g4ibbHi95aUbZGc_kKdBUJ3IH-yjdIdz/view?usp=sharing">Resume</a></li>
+        <div>
+          <p className="text-6xl font-bold mb-8">Welcome!</p>
+          <p className="text-5xl mb-8">My name is Thai, I am a <span class="typewriterEffect"><TypewriterComponent/></span></p>
+          <ul className="flex">
+            <li><a target="blank" className="link-background-content" href="https://drive.google.com/file/d/1g4ibbHi95aUbZGc_kKdBUJ3IH-yjdIdz/view?usp=sharing">Resume</a></li>
+            <li><a target="blank" className="link-background-content" href="https://www.linkedin.com/in/2770341b7/">LinkedIn</a></li>
+            <li><a target="blank" className="link-background-content" href="https://github.com/bilele123123">GitHub</a></li>
           </ul>
         </div>
       </div>
+
+    <script></script>
     </header>
   );
 }

ff6b1bb83cc6986625167fc48b25fd7e2152dc46
bilele123123 <tple06203@gmail.com>
2023-08-12 18:04:25 -0700

    Update header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,33 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <nav className="container mx-auto flex items-center justify-end font-black pb-5">
+      <nav className="container mx-auto flex items-center justify-between font-black pb-5">
+        <p className="font-black text-2xl transform hover:rotate-45 transition duration-300">Thai.</p>
         <ul className="flex space-x-4">
-          <li><a href="/" className="hover:border-b-2 hover:border-black px-2">Home</a></li>
-          <li><a href="/about" className="hover:border-b-2 hover:border-black px-2">About</a></li>
-          <li><a href="/experience" className="hover:border-b-2 hover:border-black px-2">Experience</a></li>
-          <li><a href="/projects" className="hover:border-b-2 hover:border-black px-2">Projects</a></li>
-          <li><a href="/contact" className="hover:border-b-2 hover:border-black px-2">Contact</a></li>
+          <li><a href="/" className="nav-links">Home</a></li>
+          <li><a href="/about" className="nav-links">About</a></li>
+          <li><a href="/experience" className="nav-links">Experience</a></li>
+          <li><a href="/projects" className="nav-links">Projects</a></li>
+          <li><a href="/contact" className="nav-links">Contact</a></li>
         </ul>
+        <div>
+          <ul className="flex">
+            <li><a target="blank" className="link-background" href="https://www.linkedin.com/in/2770341b7/"><i class='bx bxl-linkedin'></i></a></li>
+            <li><a target="blank" className="link-background" href="https://github.com/bilele123123/"><i class='bx bxl-github' ></i></a></li>
+            <li><a target="blank" className="link-background" href="mailto:tple06203@gmail.com"><i class='bx bx-envelope' ></i></a></li>
+          </ul>
+        </div>
       </nav>
       <div className="border-t border-gray-300"></div>
       <div className="flex items-center justify-between p-4">
         <div>
-        <img src="assets/my_picture.png" alt="Profile"/>
+        <img src="https://github.com/bilele123123/Portfolio/blob/website-revamp/assets/my_picture.png?raw=true" alt="Profile" className="w-300 h-300 rounded-full"/>
         </div>
         <div className="text-center">
-          <p className="text-xl font-bold">Welcome! I am Thai, a Software Developer.</p>
-          <ul className="flex space-x-4">
-            <li><a target="blank" href="https://drive.google.com/file/d/1g4ibbHi95aUbZGc_kKdBUJ3IH-yjdIdz/view?usp=sharing">Resume</a></li>
-            <li><a target="blank" href="https://www.linkedin.com/in/2770341b7/">LinkedIn</a></li>
-            <li><a target="blank" href="https://github.com/bilele123123/">GitHub</a></li>
+          <p className="text-xl font-bold">Welcome!</p>
+          <p className="text-xl font-bold">My name is Thai, a Software Developer.</p>
+          <ul>
+            <li><a target="blank" className="link-background" href="https://drive.google.com/file/d/1g4ibbHi95aUbZGc_kKdBUJ3IH-yjdIdz/view?usp=sharing">Resume</a></li>
           </ul>
         </div>
       </div>

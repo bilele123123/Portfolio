2b33cba5be52d3d976c9cb22d5f0490b03bd25be
bilele123123 <tple06203@gmail.com>
2024-07-19 18:13:39 -0700

    Addition: hero component for new portfolio website.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        black: {
+          DEFAULT: '000',
+          100: '#121212',
+          200: '#282828',
+          300: '#3f3f3f',
+          400: '#575757',
+          500: '#717171',
+          600: '#8b8b8b',
+        }
+      }
     },
   },
   plugins: [],
